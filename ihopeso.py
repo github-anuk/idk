c029f5e5822b41387ddba2fb5257666f5070ac9c
anuk_JetLearner <160622798+github-anuk@users.noreply.github.com>
2025-10-04 14:45:16 +0300

    ihopeso.py

--- ihopeso.py
+++ ihopeso.py
@@ -67,6 +67,7 @@ st.markdown("""
         background-size: contain;
         background-position: center;
         background-repeat: no-repeat;
+        background-color: black;
         height: 100vh;
 
         font-family: 'Segoe UI', sans-serif;
@@ -506,6 +507,7 @@ st.markdown("""
         Built with ❤️ by Anu · Powered by Streamlit · <a href="https://github.com/your-repo" target="_blank" style="color:#00ffd5; text-decoration: none;">GitHub</a>
     </div>
 """, unsafe_allow_html=True)
+
 
 
 
